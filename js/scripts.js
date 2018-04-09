d70cbbeb31cd77c487e15e8706fee0abd83d0173
Brea Borlas <breatortilla@gmail.com>
2018-04-09 10:27:18 -0700

    Add checkpoint before first breakout coding session

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// Backend Logic
 function Gem(type){
   this.type = type;
   this.pointVal = 50;
@@ -7,16 +8,16 @@ var gem1 = new Gem ("red");
 var gem2 = new Gem ("blue");
 
 function Board() {
-  this.col1 = [];
-  this.col2 = [];
-  this.col3 = [];
+  this.col1 = [gem1, gem2, gem1];
+  this.col2 = [gem2, gem1, gem2];
+  this.col3 = [gem1, gem2, gem1];
 }
 
 Board.prototype.genGem = function () {
 
 };
 
-
+// User Interface Logic
 $(document).ready(function(){
 
 });
