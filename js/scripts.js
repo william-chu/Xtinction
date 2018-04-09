87a5ba559f01153e2de799da508ac55f1eda4c66
Cory West-Forbes and Kristen Chellis <kristen@>
2018-04-09 11:17:11 -0700

    refactor match func, change var name to column and update board func

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,11 @@ var gem1 = new Gem ("red");
 var gem2 = new Gem ("blue");
 
 function Board() {
+  // this.board= []
+  // for (var i = 0; i < l; i++) {
+  //   this.board.push([]);
+  // }
+  // return this.board;
   this.board = [[gem1, gem2, gem1], [gem1, gem1, gem2], [gem2, gem2, gem1]];
 }
 
@@ -18,15 +23,15 @@ Board.prototype.genGem = function () {
 Board.prototype.match = function () {
   for (var i = 0; i < this.board.length; i++) {
     for (var j = 0; j < this.board.length; j++) {
-      var temp = this.board[i];
+      var column = this.board[i];
       if (i < this.board.length - 2) {
-        var temp1 = this.board[i + 1];
-        var temp2 = this.board[i + 2];
-        if (temp[j] === temp1[j] && temp[j] === temp2[j]) {
+        var column1 = this.board[i + 1];
+        var column2 = this.board[i + 2];
+        if (column[j] === column1[j] && column[j] === column2[j]) {
           return true;
         }
       }
-      if (temp[j] === temp[j + 1] && temp[j] === temp[j + 2] ) {
+      if (column[j] === column[j + 1] && column[j] === column[j + 2] ) {
         return true;
       }
     }
@@ -35,9 +40,9 @@ Board.prototype.match = function () {
 };
 
 Board.prototype.isValid = function () {
-  var tempBoard = Object.assign({}, this);
-  console.log(tempBoard);
-  // tempBoard.swapGems();
+  var columnBoard = Object.assign({}, this);
+  console.log(columnBoard);
+  // columnBoard.swapGems();
   this.match();
 };
 
