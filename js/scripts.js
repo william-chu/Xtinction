3cdcf3cf24be0b0857908da788f6b943b2ee604c
Cory West-Forbes and Kristen Chellis <cory@>
2018-04-09 11:09:19 -0700

    add match function to eval possible moves and isValid func to run match func

--- js/scripts.js
+++ js/scripts.js
@@ -8,14 +8,44 @@ var gem1 = new Gem ("red");
 var gem2 = new Gem ("blue");
 
 function Board() {
-  this.col1 = [gem1, gem2, gem1];
-  this.col2 = [gem2, gem1, gem2];
-  this.col3 = [gem1, gem2, gem1];
+  this.board = [[gem1, gem2, gem1], [gem1, gem1, gem2], [gem2, gem2, gem1]];
 }
 
 Board.prototype.genGem = function () {
 
 };
+
+Board.prototype.match = function () {
+  for (var i = 0; i < this.board.length; i++) {
+    for (var j = 0; j < this.board.length; j++) {
+      var temp = this.board[i];
+      if (i < this.board.length - 2) {
+        var temp1 = this.board[i + 1];
+        var temp2 = this.board[i + 2];
+        if (temp[j] === temp1[j] && temp[j] === temp2[j]) {
+          return true;
+        }
+      }
+      if (temp[j] === temp[j + 1] && temp[j] === temp[j + 2] ) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
+
+Board.prototype.isValid = function () {
+  var tempBoard = Object.assign({}, this);
+  console.log(tempBoard);
+  // tempBoard.swapGems();
+  this.match();
+};
+
+
+
+var gameBoard = new Board();
+gameBoard.isValid();
+console.log(gameBoard.match());
 
 // User Interface Logic
 $(document).ready(function(){
