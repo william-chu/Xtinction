7267ee6ec51230523b507de97633b9c2bbd46ebc
Brea Borlas <breatortilla@gmail.com>
2018-04-09 10:06:59 -0700

    Add Gem and Board constructors, empty genGem prototype, default gems

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,18 @@ function Gem(type){
   this.pointVal = 50;
 }
 
+var gem1 = new Gem ("red");
+var gem2 = new Gem ("blue");
 
+function Board() {
+  this.col1 = [];
+  this.col2 = [];
+  this.col3 = [];
+}
 
+Board.prototype.genGem = function () {
 
+};
 
 
 $(document).ready(function(){
